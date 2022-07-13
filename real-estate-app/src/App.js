55a9e4bfcd5232ecd6a27313e79be5c37964a596
José Nascimento <vittorr.nascimento@gmail.com>
2022-07-13 16:35:23 -0300

    Project- React II

--- real-estate-app/src/App.js
+++ real-estate-app/src/App.js
@@ -1,10 +1,17 @@
-import Navbar from './components/Navbar';
+
+import React from 'react'
+import Hero from './components/hero/Hero';
+import Navbar from './components/navbar/Navbar';
+import Best from './components/best/Best';
+
 
 function App() {
   return (
     <div>
         <>
          <Navbar/>
+         <Hero/>
+         <Best/>
         </>
     </div>
   );
