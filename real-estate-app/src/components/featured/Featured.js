ffb3b5d7dc902295d1005b8dfc900a794796812c
José Nascimento <vittorr.nascimento@gmail.com>
2022-07-14 14:35:43 -0300

    Project- React IV

--- real-estate-app/src/components/featured/Featured.js
+++ real-estate-app/src/components/featured/Featured.js
@@ -6,13 +6,22 @@ import Bed1 from '../../assets/bed1.jpg'
 import Bed2 from '../../assets/bed2.jpg'
 import Kitchen from '../../assets/kitchen.jpg'
 import Bathroom from '../../assets/bath1.jpg'
+
+import House2 from '../../assets/house2.jpg'
+import Bed3 from '../../assets/bed3.jpg'
+import Bed4 from '../../assets/bed4.jpg'
+import Bathroom2 from '../../assets/bath2.jpg'
+import LivingRoom from '../../assets/living-room.jpg'
+
+
+
 const Featured = () => {
     return (
         <div className='featured'>
             <h1 className='featured-text'>Top Featured Listings</h1>
-            <p>Select listings by City, State, & Zip based on views.</p>
+            <p className='featured-text' >Select listings by City, State, & Zip based on views.</p>
             <div className='container'>
-                <img src={House1} alt='' />
+                <img className='span-3 image-grid-row-2' src={House1} alt='' />
                 <img src={Bed1} alt='' />
                 <img src={Bed2} alt='' />
                 <img src={Kitchen} alt='' />
@@ -50,6 +59,48 @@ const Featured = () => {
                 </div>
             </div>
             {/* Section section */}
+            <div className='container'>
+                <img className='order-2' src={Bed3} alt='' />
+                <img className='order-3' src={Bed4} alt='' />
+
+                <img className='span-3 image-grid-row-2 order-1' src={House2} alt='' />
+
+                <img className='order-4' src={Bathroom2} alt='' />
+                <img className='order-5' src={LivingRoom} alt='' />
+
+                <div className='span-2 right-img-details order-7'>
+                    <p>A beautiful modern day home in the city with a full-size pool. Spacious and luxurious home located in Texas. Including media room, workout facility,and built-in sauna</p>
+                    <button className='btn'>View Listing</button>
+                </div>
+
+                <div className='span-3 img-details order-6'>
+                    <div className='top'>
+                        <h2>123 Acme St. Dallas, TX</h2>
+                        <p>House for Sale</p>
+                        <p className='price'>$2,677,000</p>
+                    </div>
+                    <div className='info-grid'>
+                        <div>
+                            <div className='info'>
+                                <p className='bold'>Bedrooms:</p><p>5</p>
+                            </div>
+                            <div className='info'>
+                                <p className='bold'>Bethrooms:</p><p>7</p>
+                            </div>
+
+                        </div>
+                        <div>
+                            <div className='info'>
+                                <p className='bold'>Square Feet:</p><p>8,138</p>
+                            </div>
+                            <div className='info'>
+                                <p className='bold'>Est Payment:</p><p>$14,797/mo</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
