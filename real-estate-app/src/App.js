8bc5d66e47f947af83dae090664e93e9973ffa82
José Nascimento <vittorr.nascimento@gmail.com>
2022-07-13 21:09:04 -0300

    Project- React III

--- real-estate-app/src/App.js
+++ real-estate-app/src/App.js
@@ -1,8 +1,10 @@
 
-import React from 'react'
+import React from 'react';
+import Featured from './components/featured/Featured'
+import Best from './components/best/Best';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
-import Best from './components/best/Best';
+
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
          <Navbar/>
          <Hero/>
          <Best/>
+         <Featured/>
         </>
     </div>
   );
