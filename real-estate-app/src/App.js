b71679bc5b26e4a8fb84990d0cf4ca2d2b91bc67
José Nascimento <vittorr.nascimento@gmail.com>
2022-07-19 23:57:20 -0300

    Project React IX

--- real-estate-app/src/App.js
+++ real-estate-app/src/App.js
@@ -5,11 +5,18 @@ import Best from './components/best/Best';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer'
+import Helmet from 'react-helmet';
 
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8"/>
+        <title>StarBooks</title>
+        <link rel="canonical" href="http:mysite.com/example"/>
+        <meta name="description" content="Testing text"/>
+      </Helmet>
         <>
          <Navbar/>
          <Hero/>
